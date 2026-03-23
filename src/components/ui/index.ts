b300ef8bb1af72ff7b376b8571617a09d8701259
gagankarthik <gagankarthik123@gmail.com>
2026-03-23 12:13:19 -0400

    Removed code

--- src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './Button'
-export { default as Input } from './Input'
-export { default as Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card'
-export { default as EmptyState } from './EmptyState'
-export { default as DashboardLayout } from './DashboardLayout'
